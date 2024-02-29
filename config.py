a62b1cab43b9a59029bd46a29b74cd60905d3deb
Benjamin Lorenz <benjamin.lorenz@mongodb.com>
2024-02-29 16:45:04 +0100

    initial commit

---
+++ config.py
@@ -0,0 +1,40 @@
+#
+# Copyright (c) 2024 Benjamin Lorenz
+#
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+
+    # MongoDB Atlas
+    MONGO_URI = 'mongodb+srv://mediademouser:mediademouserpassword@ist-shared.n0kts.mongodb.net/' + \
+        '?retryWrites=true&w=majority'
+    #MONGO_URI = 'mongodb+srv://watch:w741.-gs@diary.vgtmh.mongodb.net/main' + \
+    #    '?retryWrites=true&w=majority'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    # cookie security
+    SESSION_COOKIE_SECURE    = True
+    REMEMBER_COOKIE_SECURE   = True
+    REMEMBER_COOKIE_HTTPONLY = True
+
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
+
+config = {
+    'development' : DevelopmentConfig,
+    'production'  : ProductionConfig
+}
---
+++ manage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.11
+# Copyright (c) 2024 Benjamin Lorenz
+#
+
+import os
+from app import create_app, mongo
+
+flask_config = os.getenv('FLASK_CONFIG') or 'production'
+print('Using Flask config: ' + flask_config)
+app = create_app(flask_config)
+
+
+if __name__ == '__main__':
+    manager.run()
