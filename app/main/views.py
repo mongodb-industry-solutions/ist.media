3b7535931dbec45d1d068b0563cb46639fe0dc2e
Benjamin Lorenz <ben@pocket.services>
2024-03-17 11:36:47 +0100

    Main search form is now also Vector Search enabled, new main menu item Insights, lots of smaller and larger bug fixes

--- app/main/views.py
+++ app/main/views.py
@@ -37,8 +37,10 @@ collection = client[dbName][collectionName]
 gen_ai_cache_collectionName = "gen_ai_cache"
 gen_ai_cache_collection = client[dbName][gen_ai_cache_collectionName]
 
-MAX_DOCS = 8     # used for page views
-MAX_DOCS_VS = 30 # used for vector search
+MAX_DOCS_VS = 30  # number of results for vector search
+MAX_DOCS = 8      # number of articles on the home page
+MAX_RCOM = 3      # number of recommended articles
+
 
 vector_search = MongoDBAtlasVectorSearch.from_connection_string(
     MONGO_URI,
@@ -48,24 +50,24 @@ vector_search = MongoDBAtlasVectorSearch.from_connection_string(
 )
 
 
-def calculate_recommendations(text, history):
-    vs_results = vector_search.similarity_search(query=text, k=MAX_DOCS_VS)
-    rcom = map(lambda r: r.metadata, vs_results)
-    # don't recommend historic items
-    rcom = list(filter(lambda r: not r['uuid'] in history, rcom))
-    return rcom # TODO: These documents lack the 'text' field - WHY?
+def calculate_recommendations(text: str, history: list[str], count=MAX_DOCS_VS) -> list[dict]:
+    results = vector_search.similarity_search(query=text, k=MAX_DOCS_VS)
+    docs = map(lambda r: r.metadata, results) # docs lack the 'text' field - WHY?
+    # don't recommend history items, and limit to count
+    return list(filter(lambda r: not r['uuid'] in history, docs))[:count]
 
 
-def calculate_keywords(text):
+def calculate_keywords(text: str) -> list[str]:
     lcdocs = [ Document(page_content=text, metadata={"source": "local"}) ]
-    prompt_template = """Given the context of the media article, please provide
-    me with 6 keywords that capture the essence of the content and help
+    prompt_template = """Return a machine-readable Python list.
+    Given the context of the media article, please provide
+    me with 6 short keywords that capture the essence of the content and help
     finding the article while searching the web. Consider terms
     that are central to the article's subject and are likely to be imported for
     summarization. Please prioritize names of companies, names of persons,
     names of products, events, technical terms, business terms
     over general words.
-    Return as a machine-readable Python list.
+    Return a machine-readable Python list.
     "{text}"
     KEYWORDS:"""
     try:
@@ -86,7 +88,7 @@ def calculate_keywords(text):
     return keywords
 
 
-def calculate_insights(text):
+def calculate_insights(text: str) -> str:
     lcdocs = [ Document(page_content=text, metadata={"source": "local"}) ]
     prompt_template = """Tell me about the following, writing three paragraphs:
     "{text}"
@@ -105,7 +107,7 @@ def calculate_insights(text):
     return insights
 
 
-def calculate_using_rag(question):
+def calculate_using_rag(question: str) -> str:
     template = """Answer the question based only on the following context:
     {context}
 
@@ -129,9 +131,22 @@ def calculate_using_rag(question):
     return answer
 
 
-@main.route('/delete_history', methods=['POST'])
-def delete_history():
+def capitalize(text: str) -> str:
+    return ' '.join([w.title() if w.islower() else w for w in text.split()])
+
+
+@main.route('/delete_articles_history', methods=['POST'])
+def delete_articles_history():
     session['history'] = []
+    return redirect('/backstage')
+
+
+@main.route('/delete_insights_history_item/<id>', methods=['GET'])
+def delete_insights_history_item(id):
+    try:
+        gen_ai_cache_collection.delete_one({ "_id" : ObjectId(id) })
+    except Exception as e:
+        pass
     return redirect('/backstage')
 
 
@@ -139,9 +154,9 @@ def delete_history():
 def index():
     query = request.args.get('query')
     if query and query != "":
-        pat = re.compile(query, re.I)
-        docs = collection.find({ "thread.site" : "bnnbreaking.com",
-                                 "text" : {'$regex': pat}}).limit(MAX_DOCS)
+        docs = calculate_recommendations(query.strip(), [], MAX_DOCS)
+        # for unknown reasons, these docs lack the 'text' field - refetching...
+        docs = list(map(lambda doc: collection.find_one({ "uuid" : doc['uuid'] }), docs))
     else: # the start page, called without query parameter
         if 'history' in session and len(session['history']) > 0:
             history_docs = list(map(lambda uuid:
@@ -154,12 +169,9 @@ def index():
                 concatenated_titles += ("" if i == 0 else " ") + doc['title']
                 i += 1
             print("[DEBUG]: Personalization with doc titles: " + concatenated_titles)
-            docs = calculate_recommendations(concatenated_titles, session['history'])
+            docs = calculate_recommendations(concatenated_titles, session['history'], MAX_DOCS)
             # for unknown reasons, these docs lack the 'text' field - refetching...
-            docs = list(map(lambda doc:
-                            collection.find_one({ "uuid" : doc['uuid'] }), docs))
-            if len(docs) > MAX_DOCS:
-                docs = docs[:MAX_DOCS]
+            docs = list(map(lambda doc: collection.find_one({ "uuid" : doc['uuid'] }), docs))
         else: # no personalization possible - shuffle some items to start with
             docs = collection.aggregate([
                 { "$match": { "thread.site" : "bnnbreaking.com" } },
@@ -177,11 +189,12 @@ def post():
     style = request.args.get('style')
     query = request.args.get('query')
     uuid = request.args.get('uuid')
-
     if query and query != "":
         return index()
     if style and style == "summary":
         doc = collection.find_one({ "uuid" : session['uuid'] })
+        if not doc:
+            return render_template('404.html')
         sdate = doc["published"]
         fdate = datetime.fromisoformat(sdate).strftime("%a %d %b %Y %H:%M")
         fdoc=doc['text']
@@ -206,8 +219,9 @@ def post():
         except Exception as e:
             fdoc = "OpenAI crashed - you should try again (later)"
             print(e) # will be printed in the log file that is residing in /tmp
-        rcom = calculate_recommendations(doc['text'], session['history'])
-        return render_template('post.html', doc=doc, fdate=fdate, fdoc=fdoc, rcom=rcom)
+        recommendations = calculate_recommendations(doc['text'], session['history'], MAX_RCOM)
+        return render_template('post.html', doc=doc, fdate=fdate,
+                               fdoc=fdoc, recommendations=recommendations)
     else:
         if uuid: # highest prio: use uuid page parameter, if provided
             doc = collection.find_one({ "uuid" : uuid })
@@ -218,6 +232,8 @@ def post():
                 { "$match": { "thread.site" : "bnnbreaking.com" } },
                 { "$sample": { "size": 1 } }
             ]))[0]
+        if not doc:
+            return render_template('404.html')
         sdate = doc["published"]
         fdate = datetime.fromisoformat(sdate).strftime("%a %d %b %Y %H:%M")
         fdoc = html(doc['text'].replace("\n", "\n\n"))
@@ -230,9 +246,9 @@ def post():
         if len(session['history']) > 30: # limit the max length of history
             session['history'] = session['history'][-30:]
         keywords = calculate_keywords(doc['text'])
-        rcom = calculate_recommendations(doc['text'], session['history'])
+        recommendations = calculate_recommendations(doc['text'], session['history'], MAX_RCOM)
         return render_template('post.html', doc=doc, fdate=fdate, fdoc=fdoc,
-                               rcom=rcom, keywords=keywords)
+                               recommendations=recommendations, keywords=keywords)
 
 
 @main.route('/backstage')
@@ -253,22 +269,15 @@ def about():
 
 @main.route('/insights')
 def insights():
-    query = request.args.get('query')
-    if query and query != "":
-        content = html(calculate_insights(query))
-        title = ' '.join([w.title() if w.islower() else w for w in query.split()])
-        return render_template('insights.html', html_title="AI-Generated Insights",
-                               title=title, content=content)
-    else:
-        return render_template('insights.html', html_title="AI-Generated Insights",
-                               title="No Title", content="No insights.")
-
-
-@main.route('/rag')
-def rag():
+    keyword = request.args.get('keyword')
     _id = request.args.get('_id')
     query = request.args.get('query')
-    if _id and _id != "":
+    if keyword and keyword != "":
+        content = html(calculate_insights(keyword))
+        title = capitalize(keyword)
+        return render_template('insights.html', placeholder="AI-Generated Insights (RAG)",
+                               title=title, content=content)
+    elif _id and _id != "":
         try:
             cached_entry = gen_ai_cache_collection.find_one({ "_id" : ObjectId(_id) })
         except Exception as e:
@@ -276,10 +285,11 @@ def rag():
                              "answer" : "Error reading cached Q/A"}
             print(e) # will be printed in the log file that is residing in /tmp
         content = html(cached_entry['answer'])
-        title = ' '.join([w.title() if w.islower() else w for w in cached_entry['question'].split()])
-        return render_template('rag.html', placeholder="AI-Generated Insights (RAG)",
+        title = capitalize(cached_entry['question'])
+        return render_template('insights.html', placeholder="AI-Generated Insights (RAG)",
                                title=title, content=content)
     elif query and query != "":
+        query = query.strip()
         cached_entry = None
         try:
             cached_entry = gen_ai_cache_collection.find_one({ "question" : query })
@@ -287,14 +297,14 @@ def rag():
             print(e) # will be printed in the log file that is residing in /tmp
         if cached_entry:
             content = html(cached_entry['answer'])
-            title = ' '.join([w.title() if w.islower() else w for w in cached_entry['question'].split()])
+            title = capitalize(cached_entry['question'])
         else:
             content = html(calculate_using_rag(query))
-            title = ' '.join([w.title() if w.islower() else w for w in query.split()])
-        return render_template('rag.html', placeholder="AI-Generated Insights (RAG)",
+            title = capitalize(query)
+        return render_template('insights.html', placeholder="AI-Generated Insights (RAG)",
                                title=title, content=content)
     else:
-        return render_template('rag.html', placeholder="AI-Generated Insights (RAG)",
+        return render_template('insights.html', placeholder="AI-Generated Insights (RAG)",
                                title="AI-Generated Insights (RAG)",
                                content="""<p>Please enter your question in the form above.
                                Answer will be provided based only on the documents
