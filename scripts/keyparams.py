5ec3d20b8702f2859c9b3f9c679cc8b94d7adaed
Benjamin Lorenz <benjamin.lorenz@mongodb.com>
2024-04-17 17:53:57 +0200

    Added a simple script to clone the news database

--- scripts/keyparams.py
+++ scripts/keyparams.py
@@ -3,3 +3,6 @@ import os
 webzio_token   = os.environ['WEBZIO_API_KEY']
 openai_api_key = os.environ['OPENAI_API_KEY']
 MONGO_URI      = os.environ['MONGODB_IST_MEDIA']
+
+# for clonedb.py script only
+MONGO_URI_NEW  = os.environ['MONGODB_IST_MEDIA_NEW']
