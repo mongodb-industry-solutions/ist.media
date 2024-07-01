bbec397e2ce5ba3ce028e281d059631c8c884bde
Benjamin Lorenz <benjamin.lorenz@mongodb.com>
2024-07-01 17:54:36 +0200

    Refactoring for using service calls

--- config.py
+++ config.py
@@ -12,17 +12,28 @@ class Config:
     # MongoDB Atlas
     MONGO_URI = os.environ['MONGODB_IST_MEDIA']
 
+    # currently supported LLMs
+    AVAILABLE_LLMS = { 'OpenAI GPT-3.5' : 'gpt-3.5-turbo',
+                       'OpenAI GPT-4'   : 'gpt-4-turbo',
+                       'OpenAI GPT-4o'  : 'gpt-4o' }
+
     @staticmethod
     def init_app(app):
         pass
 
 
 class DevelopmentConfig(Config):
+
+    API_BASE_URL = 'http://localhost:9090/api'
+
+    # turn Flask into debug mode
     DEBUG = True
-    UPLOAD_FOLDER = '/home/bjjl/uploads'
 
 
 class ProductionConfig(Config):
+
+    API_BASE_URL = 'https://ist.media/api'
+
     # cookie security
     SESSION_COOKIE_SECURE    = True
     REMEMBER_COOKIE_SECURE   = True
