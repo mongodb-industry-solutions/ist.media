ad0bafa759a9f8c12cd7a5753198d2db36c917a6
Benjamin Lorenz <ben@pocket.services>
2024-09-24 11:41:31 +0200

    adjusted number of iterations

--- scripts/news_collect.py
+++ scripts/news_collect.py
@@ -29,7 +29,7 @@ data = response.json()
 
 if response.status_code == 200:
     collection.delete_many({}) # erase incoming
-    for article in data['response']['results'][:5]:
+    for article in data['response']['results'][:9]:
         imageURL = None
         bodyHTML = ""
         rawText = ""
