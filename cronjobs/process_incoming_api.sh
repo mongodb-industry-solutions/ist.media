bf3a1399e691783a479658eeb4157eb5eef51668
Benjamin Lorenz <mail@benjaminlorenz.com>
2025-05-15 17:44:35 +0200

    process news created with api

---
+++ cronjobs/process_incoming_api.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+. /usr/local/share/rt/bin/activate
+cd /usr/local/share/ist.media/scripts
+python delete_non_api.py
+python create_images.py
+cd /var/tmp/images.ist.media
+for i in *.png; do
+  magick "$i" "${i%.png}.webp"
+done
+cp *.webp /usr/local/share/content/images
+rm *.webp *.png
+cd /usr/local/share/ist.media/scripts
+python process_incoming.py
+python vectorize.py
+python add_sentiment.py
+python add_sections.py
+python delete_api.py
